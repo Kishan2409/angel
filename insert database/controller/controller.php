afbe10ae0209837b574191da5c0d71a0702af13c
Sinojiya Kishan <112260133+Kishan2409@users.noreply.github.com>
2023-03-02 15:00:03 +0530

    Add files via upload

--- insert database/controller/controller.php
+++ insert database/controller/controller.php
@@ -6,80 +6,24 @@ class controller extends model
     {
         parent::__construct();
         $user = $this->select1('user');
+        //$ch=$this->select('user','phone');
 
-        if (!empty($_POST['phone'])) {
+        if (isset($_POST['check_submit_btn'])) {
             $phone = $_POST['phone'];
-            $chkphone = $this->select('user', $phone);
-            if ($chkphone != $phone) {
-                if (isset($_POST["submit"])) {
-                    $fn = $_POST["fn"];
-                    $ln = $_POST["ln"];
-                    $dob = $_POST["dob"];
-                    $gender = $_POST["gender"];
-                    $email = $_POST["email"];
-
-                    $phone = $_POST["phone"];
-
-                    $pass = base64_encode($_POST["pass"]);
-
-                    // checkbox
-                    $sub = $_POST["sub"];
-                    $sub1 = "";
-                    foreach ($sub as $sub2) {
-                        $sub1 .= $sub2 . ",";
-                    }
-
-                    //uplode photo
-                    $tmp_name = $_FILES["ph"]["tmp_name"];
-                    $path = "uploads/" . $_FILES["ph"]["name"];
-                    // $imageFileType = strtolower(pathinfo($path, PATHINFO_EXTENSION));
-                    move_uploaded_file($tmp_name, $path);
-
-
-                    $data = array("fn" => $fn, "ln" => $ln, "dob" => $dob, "gender" => $gender, "email" => $email, "phone" => $phone, "pass" => $pass, "subject" => $sub1, "photo" => $path);
-                    $chk = $this->insert('user', $data);
-                    if ($chk) {
-                        echo "<script>
-                alert('User Insert Successfully')
-                window.location='./';
-                    </script>";
-                    } else {
-                        echo "<script>
-                alert('Something was wrong!')
-                window.location='./';
-                </script>";
-                    }
-                }
-            } else { ?>
-
-                <script src='https://www.tutorialspoint.com/jquery/jquery-3.6.0.js'></script>
-                <script>
-                    $(function () {
-                       
-
-                            $('#demo').html('Phone Number is already store in database');
-                        
-                    });
-                </script>
-
-            <?php }
-
-        }
-        if (isset($_GET['updateuser'])) {
-            $id = $_GET['updateuser'];
-            $fatchuser = $this->user('user', $id);
+            $phfatch = "SELECT * FROM user WHERE phone='$phone' ";
+            $phfatchrun = mysqli_query($this->con, $phfatch);
+            if (mysqli_num_rows($phfatchrun) > 0) {
+                echo "Phone Number is already store in database";
+            }
         }
 
-        if (isset($_POST['updateud'])) {
-            $id = $_GET['updateuser'];
+        if (isset($_POST["submit"])) {
             $fn = $_POST["fn"];
             $ln = $_POST["ln"];
             $dob = $_POST["dob"];
             $gender = $_POST["gender"];
             $email = $_POST["email"];
-
             $phone = $_POST["phone"];
-
             $pass = base64_encode($_POST["pass"]);
 
             // checkbox
@@ -93,6 +37,52 @@ class controller extends model
             $tmp_name = $_FILES["ph"]["tmp_name"];
             $path = "uploads/" . $_FILES["ph"]["name"];
             // $imageFileType = strtolower(pathinfo($path, PATHINFO_EXTENSION));
+            move_uploaded_file($tmp_name, $path);
+
+
+            $data = array("fn" => $fn, "ln" => $ln, "dob" => $dob, "gender" => $gender, "email" => $email, "phone" => $phone, "pass" => $pass, "subject" => $sub1, "photo" => $path);
+            $chk = $this->insert('user', $data);
+            if ($chk) {
+                echo "<script>
+                alert('User Insert Successfully')
+                window.location='./';
+                    </script>";
+            } else {
+                echo "<script>
+                alert('Something was wrong!')
+                window.location='./';
+                </script>";
+            }
+
+        }
+
+        
+        //update user
+        if (isset($_GET['updateuser'])) {
+            $id = $_GET['updateuser'];
+            $fatchuser = $this->user('user', $id);
+        }
+
+        if (isset($_POST['updateud'])) {
+            $id = $_GET['updateuser'];
+            $fn = $_POST["fn"];
+            $ln = $_POST["ln"];
+            $dob = $_POST["dob"];
+            $gender = $_POST["gender"];
+            $email = $_POST["email"];
+            $phone = $_POST["phone"];
+            $pass = base64_encode($_POST["pass"]);
+
+            // checkbox
+            $sub = $_POST["sub"];
+            $sub1 = "";
+            foreach ($sub as $sub2) {
+                $sub1 .= $sub2 . ",";
+            }
+
+            //uplode photo
+            $tmp_name = $_FILES["ph"]["tmp_name"];
+            $path = "uploads/" . $_FILES["ph"]["name"];
             move_uploaded_file($tmp_name, $path);
 
             $chk = $this->updprofile('user', $fn, $ln, $dob, $gender, $email, $phone, $pass, $sub1, $path, $id);
@@ -109,6 +99,7 @@ class controller extends model
             }
         }
 
+        //delete
         if (isset($_GET["deleteuser"])) {
             $deleteid = $_GET["deleteuser"];
             $id = array("id" => $deleteid);
@@ -125,6 +116,7 @@ class controller extends model
                 </script>";
             }
         }
+
         // load
         if (isset($_SERVER["PATH_INFO"])) {
             switch ($_SERVER["PATH_INFO"]) {
@@ -150,7 +142,7 @@ class controller extends model
                     break;
 
                 default:
-                    echo "404";
+
                     break;
             }
         }
