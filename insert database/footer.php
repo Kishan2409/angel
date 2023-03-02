50f30d5db8b3a385e08e02b8e8e566a3af2a028a
Sinojiya Kishan <112260133+Kishan2409@users.noreply.github.com>
2023-03-02 15:01:02 +0530

    Add files via upload

--- insert database/footer.php
+++ insert database/footer.php
@@ -1,33 +1,41 @@
 <script>
   $(function () {
+    $(".chaking").keyup(function(e){
+      var p=$(".chaking").val();
+      //
+      $.ajax({
+        type: "POST",
+        url: "controller.php",
+        data: {
+          "check_submit_btn":1,
+          "phone":p
 
-    $("#sub").click(function () {
-
-     
-
-      $.holdReady( true );
-      $("#commentForm").validate({
-       
-        rules: {
-        fn: "required",
-        ln: "required",
-        dob: "required",
-        email: "required",
-        phone: {
-          required: true,
-          digits: true
         },
-        ph: { accept: "image/*" }
+        success: function (response) {
+          $("#demo").text(response);
+        }
+      });
+    })
+    $("#sub").click(function () {
+      $("#commentForm").validate({
 
-      }
-
+        rules: {
+          fn: "required",
+          ln: "required",
+          dob: "required",
+          email: "required",
+          phone: {
+            required: true,
+            digits: true,
+          },
+          ph: { accept: "image/*" }
+        }
       });
 
-  })
 
 
-
-  })
+    });
+  });
 </script>
 </div>
 </div>
